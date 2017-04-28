5b7caaefe52cd7f841071c6d3366fe8c4969baa7
jamesvas5307 <jamesvas5307@live.com>
2017-04-28 18:20:40 -0400

    Test Commit

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,15 +1,27 @@
-  function animatePoints(){
-      var points = document.getElementsByClassName("point");
-      function revealPoint (){
-      for(var i=0; i < points.length; i++){
+  var pointsArray = document.getElementsByClassName("point");
+  function animatePoints(points){
+  function revealPoint (){
+    for(var i=0; i < points.length; i++){
       points[i].style.opacity = 1;
       points[i].style.transform = "scaleX(1) translateY(0) rotate(0deg)";
       points[i].style.msTransform = "scaleX(1) translateY(0) rotate(0deg)";
-      points[i].style.Webkitransform = "scaleX(1) translateY(0) rotate(0deg)"
+      points[i].style.Webkitransform = "scaleX(1) translateY(0) rotate(0deg)";
+      }
     }
-    }
-
-
-  revealPoint();
+    revealPoint();
 
   }
+
+  window.onload = function(){
+    if(window.innerHeight > 950){
+      animatePoints(pointsArray);
+    }
+    var sellingPoints = document.getElementsByClassName("selling-points")[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    window.addEventListener('scroll',function(event){
+      if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
+        animatePoints(pointsArray);
+      }
+  }
+);
+};
