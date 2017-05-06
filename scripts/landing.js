200abebb5177016f0d369a90a9a1d14eca9e971e
jamesvas5307 <jamesvas5307@live.com>
2017-05-06 12:43:55 -0400

    Checkpoint 16 Coursework

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,24 +1,27 @@
-  var pointsArray = document.getElementsByClassName("point");
-  function revealPoint (point){
-   point.style.opacity = 1;
-   point.style.transform = "scaleX(1) translateY(0) rotate(0deg)";
-   point.style.msTransform = "scaleX(1) translateY(0) rotate(0deg)";
-   point.style.Webkitransform = "scaleX(1) translateY(0) rotate(0deg)";
+function animatePoints(){
+  function revealPoint (){
+    $(this).css({
+      opacity: 1,
+      transform: 'scaleX(1) translateY(0)'
+    });
+ }
+ $.each($('.point'),revealPoint);
  }
   function animatePoints(points){
     forEach(points,revealPoint);
   }
 
-  window.onload = function(){
-    if(window.innerHeight > 950){
-      animatePoints(pointsArray);
+  $(window).load(function(){
+    if($(window).height() > 950){
+      animatePoints();
     }
     var sellingPoints = document.getElementsByClassName("selling-points")[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-    window.addEventListener('scroll',function(event){
-      if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
-        animatePoints(pointsArray);
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+
+    $(window).scroll(function(event){
+      if($(window).scrollTop() >= scrollDistance){
+        animatePoints();
       }
-  }
+    });
 );
-};
+});
